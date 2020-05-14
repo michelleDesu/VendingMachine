906451cfa032eef07a9bacc7d48eeba9220da26d
michelle <michelle.johansson85@gmail.com>
2020-05-14 09:02:20 +0200

    VendingMachineImpl.java: * added class for VendingMachineImpl, empty body with only methods

---
+++ src/main/java/se/lexicon/michelle/model/VendingMachineImpl.java
@@ -0,0 +1,51 @@
+package se.lexicon.michelle.model;
+
+public class VendingMachineImpl implements VendingMachine{
+    private int amount,
+                productNumber;
+
+    @Override
+    public void addCurrency(int amount) {
+
+    }
+
+    @Override
+    public Product request(int productNumber) {
+        return null;
+    }
+
+    @Override
+    public int endSession() {
+        return 0;
+    }
+
+    @Override
+    public String getDescription(int productNumber) {
+        return null;
+    }
+
+    @Override
+    public int getBalance() {
+        return 0;
+    }
+
+    @Override
+    public String[] getProducts() {
+        return new String[0];
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
