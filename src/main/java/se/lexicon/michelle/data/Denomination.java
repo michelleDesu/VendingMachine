deca90ae47d2aa1fb240053932288dd3e7325542
michelle <michelle.johansson85@gmail.com>
2020-05-14 08:28:09 +0200

    Denomination.java: * Enum type was added

---
+++ src/main/java/se/lexicon/michelle/data/Denomination.java
@@ -0,0 +1,16 @@
+package se.lexicon.michelle.data;
+
+public enum Denomination {
+        KR1,
+        KR2,
+        KR3,
+        KR5,
+        KR10,
+        KR20,
+        KR50,
+        KR100,
+        KR200,
+        KR500,
+        KR1000;
+
+}
