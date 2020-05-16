da3a43776e42ea2253e8e9f5625f7b0ca4a3c0df
michelle <michelle.johansson85@gmail.com>
2020-05-16 09:51:03 +0200

    IVikingWeapon.java: * Added interface for viking weapons

---
+++ src/main/java/se/lexicon/michelle/interfaces/IVikingWeapon.java
@@ -0,0 +1,7 @@
+package se.lexicon.michelle.interfaces;
+
+public interface IVikingWeapon extends Product{
+    void setType(String type);
+    void setFunction(String function);
+    void setAmmunition(boolean ammunition);
+}
